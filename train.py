4c385b8e1cbaec2e651a127ae58f96ea744096dc
mridul3301 <mridulsharma3301@gmail.com>
2023-09-08 16:01:21 +0545

    update

--- train.py
+++ train.py
@@ -1,67 +1,88 @@
 import torch
-import albumentations as a
-from albumentations.pytorch import ToTensorV2
 from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
+from torchvision import transforms
 from unet import UNET
+from torchmetrics import JaccardIndex
 
-from utils import load_checkpoint, save_checkpoint, get_loaders, check_accuracy, save_predictions_as_imgs
+from utils import load_checkpoint, save_checkpoint, get_loaders
 
 from config import (LEARNING_RATE, DEVICE, BATCH_SIZE, NUM_WORKERS, NUM_EPOCHS, IMAGE_HEIGHT, IMAGE_WIDTH, PIN_MEMORY,
                     LOAD_MODEL, TRAIN_IMG_DIR, TRAIN_MASK_DIR, VAL_IMG_DIR, VAL_MASK_DIR)
 
 
-def train_fn(loader, model, optimizer, loss_fn, scaler):
-    loop = tqdm(loader)
+def train(train_loader, val_loader, device, optimizer, model, criteria, iou_score):
+    train_losses, val_losses, iou = [], [], []
+    for e in tqdm(range(NUM_EPOCHS)):
+        model.train()
 
-    for batch_idx, (data, targets) in enumerate(loop):
-        data = data.to(device=DEVICE)
-        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+        running_train_loss, running_val_loss, running_iou = 0, 0, 0
 
-        with torch.cuda.amp.autocast():
-            predictions = model(data)
-            loss = loss_fn(predictions, targets)
+        for i, data in enumerate(train_loader):
+            image_i, mask_i = data
+            image = image_i.to(device)
+            mask = mask_i.to(device)
 
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+            optimizer.zero_grad()
 
-        loss.set_postfix(loss=loss.item())
+            output = model(image.float())
+
+            train_loss = criteria(output.float, mask.long())
+
+            train_loss.backward()
+            optimizer.step()
+
+            running_train_loss += train_loss
+        train_losses.append(running_train_loss)
+
+        model.eval()
+        with torch.no_grad():
+            for i, data in enumerate(val_loader):
+                image_i, mask_i = data
+                image = image_i.to(DEVICE)
+                mask = mask_i.to(DEVICE)
+
+                output = model(image.float())
+
+                val_loss = criteria(output.float(), mask.long())
+                running_val_loss += val_loss.item()
+
+                iou = iou_score(output.float(), mask.long())
+                running_iou += iou.item()
+
+            val_losses.append(running_val_loss)
+            iou.append(running_iou)
+            print(f"Epoch : {e}, Train Loss : {running_train_loss}, Val Loss : {running_val_loss}, IOU : {running_iou}")
 
 
 def main():
-    train_transform = a.Compose(
+    train_transform = transforms.Compose(
         [
-            a.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
-            a.Rotate(limit=35, p=1.0),
-            a.HorizontalFlip(p=0.5),
-            a.VerticalFlip(p=0.1),
-            a.Normalize(
+            transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.1),
+            transforms.Normalize(
                 mean=[0.0, 0.0, 0.0],
                 std=[1.0, 1.0, 1.0],
-                max_pixel_value=255.0,
             ),
-            ToTensorV2(),
         ],
     )
 
-    val_transforms = a.Compose(
+    val_transforms = transforms.Compose(
         [
-            a.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
-            a.Normalize(
+            transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+            transforms.Normalize(
                 mean=[0.0, 0.0, 0.0],
                 std=[1.0, 1.0, 1.0],
-                max_pixel_value=255.0,
             ),
-            ToTensorV2(),
         ],
     )
 
     model = UNET().to(DEVICE)
-    loss_fn = nn.BCEWithLogitsLoss()
+    criteria = nn.BCEWithLogitsLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    iou_score = JaccardIndex(task="multiclass", num_classes=2)
 
     train_loader, val_loader = get_loaders(
         TRAIN_IMG_DIR,
@@ -75,29 +96,17 @@ def main():
         PIN_MEMORY,
     )
 
+    train(train_loader, val_loader, DEVICE, optimizer, model, criteria, iou_score)
+
+    # save model
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    save_checkpoint(checkpoint)
+
     if LOAD_MODEL:
         load_checkpoint(torch.load("my_checkpoint.pth"), model)
-
-    check_accuracy(val_loader, model, device=DEVICE)
-    scaler = torch.cuda.amp.GradScaler()
-
-    for epoch in range(NUM_EPOCHS):
-        train_fn(train_loader, model, optimizer, loss_fn, scaler)
-
-        # save model
-        checkpoint = {
-            "state_dict": model.state_dict(),
-            "optimizer": optimizer.state_dict(),
-        }
-        save_checkpoint(checkpoint)
-
-        # check accuracy
-        check_accuracy(val_loader, model, device=DEVICE)
-
-        # print some examples to a folder
-        save_predictions_as_imgs(
-            val_loader, model, folder="saved_images/", device=DEVICE
-        )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import torch
-import torchvision
 from dataset import SegmentationDataset
 from torch.utils.data import DataLoader
 
@@ -54,45 +53,3 @@ def get_loaders(
     )
 
     return train_loader, val_loader
-
-
-def check_accuracy(loader, model, device="cuda"):
-    num_correct = 0
-    num_pixels = 0
-    dice_score = 0
-    model.eval()
-
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device)
-            y = y.to(device).unsqueeze(1)
-            preds = torch.sigmoid(model(x))
-            preds = preds[preds > 0.5].float()
-            num_correct += preds[preds == y].sum()
-            num_pixels += torch.numel(preds)
-            dice_score += (2 * (preds * y).sum()) / (
-                    (preds + y).sum() + 1e-8
-            )
-
-    print(
-        f"Got {num_correct}/{num_pixels} with acc {num_correct / num_pixels * 100:.2f}"
-    )
-    print(f"Dice score: {dice_score / len(loader)}")
-    model.train()
-
-
-def save_predictions_as_imgs(
-        loader, model, folder="saved_images/", device="cuda"
-):
-    model.eval()
-    for idx, (x, y) in enumerate(loader):
-        x = x.to(device=device)
-        with torch.no_grad():
-            preds = torch.sigmoid(model(x))
-            preds = preds[preds > 0.5].float()
-        torchvision.utils.save_image(
-            preds, f"{folder}/pred_{idx}.png"
-        )
-        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
-
-    model.train()
